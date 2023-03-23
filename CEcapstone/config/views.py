e4b297edacd20c013746e4a0a2e2f34431571712
vittroi384 <eunsol924@naver.com>
2023-03-24 05:03:01 +0900

    restapi

---
+++ CEcapstone/config/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+import json
+# Create your views here.
+class HomeView(TemplateView):
+  template_name = 'index.html'
+
+def index(request):
+    with open('./data/data.json', 'r') as f:
+        json_dict = json.loads(f.read())
+    return render(request, 'index.html', {'json_dict': json_dict})
---
+++ CEcapstone/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  내이름은 서욱현 입니다
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
