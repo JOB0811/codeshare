d2c426ebb9a885faa8c0a6d85764eacd939934cc
jiwonmokpo <90379629+jiwonmokpo@users.noreply.github.com>
2023-03-24 16:12:31 +0900

    add camera

--- CEcapstone/config/settings.py
+++ CEcapstone/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c^za9ov8cxyrwzjkq5dkh&9&cox47bd9o#xnkyj7qpcn8ab^^&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
