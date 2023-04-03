fe493f1b934c7656de22caf791bab8afc96a909c
JOB0811 <dnjsqlsgpqls@naver.com>
2023-04-03 16:12:53 +0900

    second commit

--- CEcapstone/src/App.vue
+++ CEcapstone/src/App.vue
@@ -1,7 +1,62 @@
 <template>
   <v-app>
     <v-main>
-      <Appbar/>
+      <div class="background-image">
+    <Headerbtn/>
+          <h1 class = "letter-spacing">목포에 오신 것을 환영합니다!</h1>
+          <p class = "letter-spacing">Welcome to visit Mokpo</p>
+          <a></a><br>
+          <a></a><br>
+          <a href="#">
+          <img src="https://blog.kakaocdn.net/dn/uqJpZ/btqyenBIIXx/mh1Cc5F023UGpfQTFBdqV0/img.jpg" class="app-store-button" alt="Download on the App Store" />
+          </a>
+          <a href="#">
+          <img src="https://blog.kakaocdn.net/dn/bDTFBH/btqOhuf3oId/FMaicByKnbnp0lGXSzfJQ0/img.png" class="play-store-button" alt="Download on the play Store" />
+          </a>
+          <v-container>
+      <div class="card-container">
+        <v-card class="image-card">
+          <v-card-media
+            :src="('@/image/Mokpo.jpg')"
+            height="200px"
+          />
+          <v-card-title>{{ 관광추천 }}</v-card-title>
+          <v-card-text>{{ 추천지1 }}</v-card-text>
+        </v-card>
+        <v-card class="image-card">
+          <v-card-media
+            :src="imageSrc2"
+            height="200px"
+          />
+          <v-card-title>{{ 관광추천 }}</v-card-title>
+          <v-card-text>{{ 추천지2 }}</v-card-text>
+        </v-card>
+        <v-card class="image-card">
+          <v-card-media
+            :src="imageSrc3"
+            height="200px"
+          />
+          <v-card-title>{{ 관광추천 }}</v-card-title>
+          <v-card-text>{{ 추천지3 }}</v-card-text>
+        </v-card>
+      </div>
+    </v-container>
+    <a></a><br>
+    <a></a><br> 
+    <a></a><br> 
+    <a></a><br> 
+    <a></a><br> 
+    <a></a><br> 
+    <a></a><br> 
+    <a></a><br>
+    <a></a><br> 
+    <a></a><br> 
+    <a></a><br> 
+    <a></a><br>       
+
+
+    </div> 
+      <!-- <Appbar/> -->
       <v-container class="Component-main" fluid>
     <v-row>
       <v-col cols="4">
@@ -38,7 +93,8 @@
 </template>
 
 <script>
-import Appbar from './components/Appbar.vue'
+import Headerbtn from './components/Headerbtn.vue';
+// import Appbar from './components/Appbar.vue'
 import Componentemail from './components/Componentemail.vue'
 import Footer from './components/Footer.vue'
 import Sitemap from './components/Sitemap.vue';
@@ -51,7 +107,8 @@ export default {
   name: 'App',
 
   components: {
-    Appbar,
+    Headerbtn,
+    // Appbar,
     Componentemail,
     Componentsubscribe,
     Sitemap,
@@ -67,6 +124,63 @@ export default {
 }
 </script>
 <style>
+.background-image {
+  background-image: url('https://www.mokpo.go.kr/build/images/1517/15173024/1517302477.jpg/666x1x85/666x1_1517302477.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 300px;
+}
+
+
+
+.letter-spacing {
+    letter-spacing: 0.2em; 
+    margin-left: 50px;
+    padding-top: 40px;
+  }
+.app-store-button {
+  width: 80px;
+  height: 80px;
+  position: relative;
+  top: 20px;
+  left: 5%;
+  right: 50%;
+  bottom: 25%;
+  transform: translate(-50%, -50%);
+  border-radius: 25px;
+}
+
+.play-store-button {
+  width: 150px;
+  height: 75px;
+  position: relative;
+  top: 15px;
+  left: 8%;
+  right: 40%;
+  bottom: 20px;
+  transform: translate(-50%, -50%);
+  border-radius: 25px;
+}
+
+
+.card-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.image-card {
+  flex: 1;
+  margin-right: 40px;
+  background: rgba(255, 255, 255, 0.7);
+  padding : 80px;
+  margin-top: 12em;
+}
+
+.image-card:last-child {
+  margin-right: 0;
+}
+
+
 .component-main{
   background-image: url('https://www.socialfocus.co.kr/news/photo/201910/5296_7612_5032.jpg');
   background-repeat: no-repeat;
---
+++ CEcapstone/src/components/Headerbtn.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="header">
+      <div class="toggle-button" @click="toggleMenu">
+        <svg viewBox="0 0 100 80" width="30" height="30">
+          <rect width="100" height="10"></rect>
+          <rect y="30" width="100" height="10"></rect>
+          <rect y="60" width="100" height="10"></rect>
+        </svg>
+      </div>
+        <a class = "Hbtn">목포 지역관광</a>
+      <nav>
+        <ul>
+          <li><a href="#">홈</a></li>
+          <li><a href="#">지역 소개</a></li>
+          <li><a href="#">문의</a></li>
+        </ul>
+      </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        menuOpen: false
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.menuOpen = !this.menuOpen;
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+    background-color: #f8f8f8;
+    border-bottom: 1px solid #ccc;
+  }
+  
+  nav ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  nav li {
+    margin-right: 1rem;
+  }
+  
+  nav a {
+    color: #333;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.1rem;
+  }
+
+  a {
+    padding-left: 30px;
+  }
+
+  .Hbtn {
+    font-family: 'Nanum Gothic', sans-serif;
+  font-size: 1.1rem;
+  color: #333;
+  letter-spacing: 2px;
+  line-height: 1.5;
+  text-align: center;
+  }
+
+ 
+  </style>
